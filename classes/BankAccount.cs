82de8fd2abe0aee871ba32784c5773bd6f6de007
Casalnovo Giacomo <gc76@live.it>
2023-05-04 16:31:28 +0200

    add BankAccount.cs class

---
+++ classes/BankAccount.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Questo file conterrà la definizione di un conto bancario.
+ * La programmazione orientata agli oggetti organizza il codice creando tipi sotto forma di classi.
+ * Queste classi contengono il codice che rappresenta un'entità specifica.
+ * La classe BankAccount rappresenta un conto bancario.
+ * Il codice implementa operazioni specifiche tramite metodi e proprietà.
+ * In questa esercitazione il conto bancario supporta questo comportamento:
+
+Esiste un numero di 10 cifre che identifica in modo univoco il conto.
+Esiste una stringa in cui vengono archiviati i nomi dei titolari.
+È possibile recuperare il saldo.
+Il conto accetta versamenti.
+Il conto accetta prelievi.
+Il saldo iniziale deve essere positivo.
+I prelievi non possono comportare un saldo negativo.
+
+ */
+
+
+namespace classes
+{
+    internal class BankAccount
+    {
+    }
+}
