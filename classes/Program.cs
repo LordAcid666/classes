8e2f3e050fc966474f05db5b97df200959aa0ef0
Casalnovo Giacomo <gc76@live.it>
2023-05-04 16:25:10 +0200

    add links to program.cs

--- classes/Program.cs
+++ classes/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+// https://learn.microsoft.com/it-it/dotnet/csharp/fundamentals/tutorials/classes
+// https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/tutorials/classes
+
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
